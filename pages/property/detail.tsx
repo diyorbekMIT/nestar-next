8a35d24454b9d1e836bd88c3765c089bf6a2f474
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-10-02 21:06:08 +0900

    feat: create new comment alligned to Property

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -30,8 +30,8 @@ import 'swiper/css/pagination';
 import { GET_PROPERTIES, GET_PROPERTY } from '../../apollo/user/query';
 import { T } from '../../libs/types/common';
 import { Direction, Message } from '../../libs/enums/common.enum';
-import { LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
-import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
+import { CREATE_COMMENT, LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
+import { sweetErrorHandling, sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
 import { GET_COMMENTS } from '../../apollo/admin/query';
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
@@ -62,6 +62,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 	/** APOLLO REQUESTS **/
 
 	const [likeTargetProperty] = useMutation(LIKE_TARGET_PROPERTY);
+	const [createComment] = useMutation(CREATE_COMMENT);
 
 	const {
 		loading: getPropertyLoading,
@@ -171,6 +172,20 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 		} catch (err: any) {
             console.log("ERROR, likePropertyHandler", err.message);
 			sweetMixinErrorAlert(err.message).then();
+		}
+	}
+
+	const createCommentHandler = async () => {
+		try {
+			if (!user._id) throw new Error(Message.NOT_AUTHENTICATED);
+			await createComment({ variables:{input: insertCommentData}});
+
+			setInsertCommentData({...insertCommentData, commentContent: ''});
+
+			await getCommentsRefetch({input: commentInquiry});
+
+		} catch( err: any ) {
+			await sweetErrorHandling(err)
 		}
 	}
 
@@ -491,6 +506,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 										<Button
 											className={'submit-review'}
 											disabled={insertCommentData.commentContent === '' || user?._id === ''}
+											onClick={createCommentHandler}
 										>
 											<Typography className={'title'}>Submit Review</Typography>
 											<svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
