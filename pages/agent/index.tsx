f4375876006dd01d861cf376df142383015ab522
diyorbekMIT <diyorbekjon2202@gmail.com>
2024-10-04 19:55:05 +0900

    feat: develop agents list screen component logic

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -9,6 +9,12 @@ import AgentCard from '../../libs/components/common/AgentCard';
 import { useRouter } from 'next/router';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Member } from '../../libs/types/member/member';
+import { LIKE_TARGET_MEMBER } from '../../apollo/user/mutation';
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_AGENTS } from '../../apollo/user/query';
+import { T } from '../../libs/types/common';
+import { Messages } from '../../libs/config';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
 
 export const getStaticProps = async ({ locale }: any) => ({
 	props: {
@@ -32,6 +38,23 @@ const AgentList: NextPage = ({ initialInput, ...props }: any) => {
 	const [searchText, setSearchText] = useState<string>('');
 
 	/** APOLLO REQUESTS **/
+
+	const [likeTargetMember] = useMutation(LIKE_TARGET_MEMBER);
+
+    const {
+      loading: getAgentsLoading,
+      data: getAgentsData,
+      error: getAgentsError,
+      refetch: getAgentsRefetch,
+    } = useQuery(GET_AGENTS, {
+      fetchPolicy: 'network-only',
+      variables: { input: searchFilter },
+      notifyOnNetworkStatusChange: true,
+	  onCompleted: (data: T) => {
+		setAgents(data?.getAgents?.list);
+		setTotal(data?.getAgents?.metaCounter[0]?.total);
+  },
+});
 	/** LIFECYCLES **/
 	useEffect(() => {
 		if (router.query.input) {
@@ -53,6 +76,25 @@ const AgentList: NextPage = ({ initialInput, ...props }: any) => {
 		setSortingOpen(false);
 		setAnchorEl(null);
 	};
+
+	const likeMemberHandler = async (user: any, id: string) => {
+		try {
+		  if (!id) return;
+		  if (!user._id) throw new Error(Messages.error2);
+	  
+		  await likeTargetMember({
+			variables: {
+			  input: id,
+			},
+		  });
+	  
+		  await getAgentsRefetch({ input: searchFilter });
+		  await sweetTopSmallSuccessAlert('success', 800);
+		} catch (err: any) {
+		  console.log('ERROR, likePropertyHandler:', err.message);
+		  await sweetMixinErrorAlert(err.message).then();
+		}
+	  };
 
 	const sortingHandler = (e: React.MouseEvent<HTMLLIElement>) => {
 		switch (e.currentTarget.id) {
@@ -139,7 +181,7 @@ const AgentList: NextPage = ({ initialInput, ...props }: any) => {
 							</div>
 						) : (
 							agents.map((agent: Member) => {
-								return <AgentCard agent={agent} key={agent._id} />;
+								return <AgentCard agent={agent} likeMemberHandler={likeMemberHandler} key={agent._id} />;
 							})
 						)}
 					</Stack>
